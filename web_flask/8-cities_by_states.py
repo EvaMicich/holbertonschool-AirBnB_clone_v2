77d2f6e711ca9d711ba698c363ce9007cbeed13a
EvaMicich <6366@holbertonstudents.com>
2023-08-18 18:30:26 -0700

    updated state and city filterrs

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,14 +3,15 @@
 from flask import Flask, render_template
 from models import storage
 from models import *
+
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """ displays all states"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template("7-states_list.html", states=states)
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """ displays all cities"""
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
